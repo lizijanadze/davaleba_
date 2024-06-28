82ec1226406c7c6e7e06dc957af53f41ac64687e
lizijanadze <164235851+lizijanadze@users.noreply.github.com>
2024-06-28 19:24:38 +0400

    Add files via upload

---
+++ daveleba/script.js
@@ -0,0 +1,37 @@
+
+
+
+
+function sayhello (){
+    console.log("hello world")
+}
+
+sayhello()
+
+
+
+
+
+
+
+
+function greet (name){
+console.log("hello"+ name)
+
+}
+
+greet("lizi")
+
+
+
+
+function check_number(num){
+if(num > 10){
+    console.log( " num metia 10ze ")
+}else{
+    console.log("10ze naklebia ")
+}
+
+}
+
+check_number(12)
